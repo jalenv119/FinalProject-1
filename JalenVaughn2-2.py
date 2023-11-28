337fc4f925f9c30bce0420f4adfa15450eef3bd7
jalenv119 <150954380+jalenv119@users.noreply.github.com>
2023-11-28 20:58:51 +0000

    gg

--- JalenVaughn2-2.py
+++ JalenVaughn2-2.py
@@ -1,3 +1,6 @@
+import csv 
+import re
+
 def grade_score(score, best):
     if score >= best - 10:
         return 'A'
@@ -10,30 +13,55 @@ def grade_score(score, best):
     else:
         return 'F'
 
-def main():
-    student_amount = int(input('Total number of students:'))
-    student_scores = []
-    while len(student_scores) < student_amount:
-        try:
-            temp_student_scores = (input(f'Enter {student_amount} score(s):'))
-            temp_scores = list(map(int, temp_student_scores.split()))
-            if len(temp_scores) >= student_amount:
-                student_scores.extend(temp_scores[:student_amount])
-            else:
-                print('Try Again!')
-        except ValueError:
-            print('F')
-            
-    best = max(student_scores)
-    for i in range(student_amount):
-        score = student_scores[i]
-        grade = grade_score(score, best)
-        student_number = i + 1
-        print(f'student {student_number} score is {score} and grade is {grade}')
-
-
+def extract_student_info(row):
     
-    #print(student_scores)
+    name_match = re.search(r'(?i)studentFile', row)
+    score_match = re.search(r'(?i)scoreFile', row)
+
+    if name_match and score_match:
+        name_col = name_match.group()
+        score_col = score_match.group()
+
+        # Extract values using regex
+        name = re.search(rf'(?i){name_col}\s*,\s*(?P<name>[\w\s]+)', row).group('name')
+        score = int(re.search(rf'(?i){score_col}\s*,\s*(?P<score>\d+)', row).group('score'))
+        
+        return name, score
+    else:
+        raise ValueError()
+
+def main():
+    read_from_csv = True
+
+    if read_from_csv:
+        csv_filename = input("Enter CSV file name: ")
+        with open(csv_filename, 'r') as csvfile:
+            csv_data = csvfile.read()
+            student_info_list = [extract_student_info(row) for row in csv_data.splitlines()]
+
+    else:
+        student_amount = int(input('Total number of students: '))
+        student_info_list = []
+        while len(student_info_list) < student_amount:
+            try:
+                temp_student_info = input(f'Enter {student_amount} student info (e.g., "Name, Score"): ')
+                name, score = extract_student_info(temp_student_info)
+                student_info_list.append((name, score))
+            except ValueError as e:
+                print(f'Error: {e}')
+
+    best = max(student_info_list, key=lambda x: x[1])[1]
+    student_data = {}
+
+    for i, (name, score) in enumerate(student_info_list, start=1):
+        grade = grade_score(score, best)
+        student_data[f'student {i}'] = {'name': name, 'score': score, 'grade': grade}
+
+    for student, data in student_data.items():
+        print(f"{student} {data['name']}'s score is {data['score']} and grade is {data['grade']}")
+
+if __name__ == "__main__":
+    main()
 
 
 main()
