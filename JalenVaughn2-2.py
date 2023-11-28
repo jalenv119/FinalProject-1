326d3eea10b51afbafa2e44dffa89adaa2ee90fc
twistedttea <twistedtteavr@gmail.com>
2023-11-28 16:34:46 -0600

    on some weird stuff pt 3

--- JalenVaughn2-2.py
+++ JalenVaughn2-2.py
@@ -2,22 +2,23 @@ import csv
 from functions import *
 
 def main():
-    csv_filename = inputFileCheck()
+    csv_filename = 'input.csv'
 
     try:
+        student_data = {}
         with open(csv_filename, 'r') as csvfile:
-            student_data = csv.DictReader(csvfile)
-            for row in student_data:
-                print(row)
+            openedInputCSV = csv.DictReader(csvfile)
+            for row in openedInputCSV:
+                student_data[(row['Name'])] = row['Score'] 
 
-        best = max(int(row['score']) for row in student_data)
-        print(student_data)
-        for i, row in enumerate(student_data, start=1):
-            name = row['studentname']
-            score = int(row['score'])
-            grade = grade_score(score, best)
-            print(name,score,grade)
-            student_data[f'student {i}'] = {'name': name, 'score': score, 'grade': grade}
+            best = max(student_data.values())
+
+            for i, row in enumerate(student_data, start=1):
+                name = row['studentname']
+                score = int(row['score'])
+                grade = grade_score(score, best)
+                print(name,score,grade)
+                student_data[f'student {i}'] = {'name': name, 'score': score, 'grade': grade}
 
         with open('output.csv', 'w', newline='') as output_file:
             fieldnames = ['Student', 'Name', 'Score', 'Grade']
