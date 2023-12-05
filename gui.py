31638e86734b6b25d0c16ea66bcf61464dff8e18
jalenv119 <150954380+jalenv119@users.noreply.github.com>
2023-12-05 19:59:08 +0000

    THANK GOD

--- gui.py
+++ gui.py
@@ -1,143 +1,67 @@
-import os
-os.system("pyuic4 -o gui.py gui.ui")
-<?xml version="1.0" encoding="UTF-8"?>
-<ui version="4.0">
- <class>MainWindow</class>
- <widget class="QMainWindow" name="MainWindow">
-  <property name="geometry">
-   <rect>
-    <x>0</x>
-    <y>0</y>
-    <width>240</width>
-    <height>320</height>
-   </rect>
-  </property>
-  <property name="windowTitle">
-   <string>MainWindow</string>
-  </property>
-  <widget class="QWidget" name="centralwidget">
-   <widget class="QRadioButton" name="radioButton">
-    <property name="geometry">
-     <rect>
-      <x>30</x>
-      <y>20</y>
-      <width>62</width>
-      <height>21</height>
-     </rect>
-    </property>
-    <property name="text">
-     <string>manual input</string>
-    </property>
-   </widget>
-   <widget class="QRadioButton" name="radioButton_2">
-    <property name="geometry">
-     <rect>
-      <x>110</x>
-      <y>20</y>
-      <width>62</width>
-      <height>21</height>
-     </rect>
-    </property>
-    <property name="text">
-     <string>CSV input</string>
-    </property>
-   </widget>
-   <widget class="QLineEdit" name="lineEdit">
-    <property name="geometry">
-     <rect>
-      <x>50</x>
-      <y>60</y>
-      <width>181</width>
-      <height>20</height>
-     </rect>
-    </property>
-   </widget>
-   <widget class="QPlainTextEdit" name="plainTextEdit">
-    <property name="geometry">
-     <rect>
-      <x>0</x>
-      <y>60</y>
-      <width>61</width>
-      <height>21</height>
-     </rect>
-    </property>
-    <property name="plainText">
-     <string>user input
-</string>
-    </property>
-   </widget>
-   <widget class="QLineEdit" name="lineEdit_2">
-    <property name="geometry">
-     <rect>
-      <x>70</x>
-      <y>130</y>
-      <width>161</width>
-      <height>20</height>
-     </rect>
-    </property>
-   </widget>
-   <widget class="QPlainTextEdit" name="plainTextEdit_2">
-    <property name="geometry">
-     <rect>
-      <x>0</x>
-      <y>130</y>
-      <width>71</width>
-      <height>21</height>
-     </rect>
-    </property>
-    <property name="plainText">
-     <string>output file name</string>
-    </property>
-   </widget>
-   <widget class="QDialogButtonBox" name="buttonBox">
-    <property name="geometry">
-     <rect>
-      <x>60</x>
-      <y>260</y>
-      <width>116</width>
-      <height>17</height>
-     </rect>
-    </property>
-    <property name="standardButtons">
-     <set>QDialogButtonBox::Cancel|QDialogButtonBox::Ok</set>
-    </property>
-   </widget>
-   <widget class="QLineEdit" name="lineEdit_3">
-    <property name="geometry">
-     <rect>
-      <x>0</x>
-      <y>90</y>
-      <width>231</width>
-      <height>20</height>
-     </rect>
-    </property>
-   </widget>
-   <widget class="QLineEdit" name="lineEdit_4">
-    <property name="geometry">
-     <rect>
-      <x>-8</x>
-      <y>160</y>
-      <width>241</width>
-      <height>20</height>
-     </rect>
-    </property>
-   </widget>
-  </widget>
-  <widget class="QMenuBar" name="menubar">
-   <property name="geometry">
-    <rect>
-     <x>0</x>
-     <y>0</y>
-     <width>240</width>
-     <height>18</height>
-    </rect>
-   </property>
-  </widget>
-  <widget class="QStatusBar" name="statusbar"/>
- </widget>
- <resources/>
- <connections/>
-</ui>
+from PyQt6 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(240, 320)
+        self.centralwidget = QtWidgets.QWidget(parent=MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.radioButton = QtWidgets.QRadioButton(parent=self.centralwidget)
+        self.radioButton.setGeometry(QtCore.QRect(30, 20, 62, 21))
+        self.radioButton.setObjectName("radioButton")
+        self.radioButton_2 = QtWidgets.QRadioButton(parent=self.centralwidget)
+        self.radioButton_2.setGeometry(QtCore.QRect(110, 20, 62, 21))
+        self.radioButton_2.setObjectName("radioButton_2")
+        self.lineEdit = QtWidgets.QLineEdit(parent=self.centralwidget)
+        self.lineEdit.setGeometry(QtCore.QRect(50, 60, 181, 20))
+        self.lineEdit.setObjectName("lineEdit")
+        self.plainTextEdit = QtWidgets.QPlainTextEdit(parent=self.centralwidget)
+        self.plainTextEdit.setGeometry(QtCore.QRect(0, 60, 61, 21))
+        self.plainTextEdit.setObjectName("plainTextEdit")
+        self.lineEdit_2 = QtWidgets.QLineEdit(parent=self.centralwidget)
+        self.lineEdit_2.setGeometry(QtCore.QRect(70, 130, 161, 20))
+        self.lineEdit_2.setObjectName("lineEdit_2")
+        self.plainTextEdit_2 = QtWidgets.QPlainTextEdit(parent=self.centralwidget)
+        self.plainTextEdit_2.setGeometry(QtCore.QRect(0, 130, 71, 21))
+        self.plainTextEdit_2.setObjectName("plainTextEdit_2")
+        self.buttonBox = QtWidgets.QDialogButtonBox(parent=self.centralwidget)
+        self.buttonBox.setGeometry(QtCore.QRect(60, 260, 116, 17))
+        self.buttonBox.setStandardButtons(QtWidgets.QDialogButtonBox.StandardButton.Cancel|QtWidgets.QDialogButtonBox.StandardButton.Ok)
+        self.buttonBox.setObjectName("buttonBox")
+        self.lineEdit_3 = QtWidgets.QLineEdit(parent=self.centralwidget)
+        self.lineEdit_3.setGeometry(QtCore.QRect(0, 90, 231, 20))
+        self.lineEdit_3.setObjectName("lineEdit_3")
+        self.lineEdit_4 = QtWidgets.QLineEdit(parent=self.centralwidget)
+        self.lineEdit_4.setGeometry(QtCore.QRect(-8, 160, 241, 20))
+        self.lineEdit_4.setObjectName("lineEdit_4")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(parent=MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 240, 18))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(parent=MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.radioButton.setText(_translate("MainWindow", "manual input"))
+        self.radioButton_2.setText(_translate("MainWindow", "CSV input"))
+        self.plainTextEdit.setPlainText(_translate("MainWindow", "user input\n"
+""))
+        self.plainTextEdit_2.setPlainText(_translate("MainWindow", "output file name"))
+
+
 if __name__ == "__main__":
-    window = MyWindow()
-    sys.exit(app.exec_())
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec())
