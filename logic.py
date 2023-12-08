c45415f99571833f4eab22e805b21282eef6e85b
twistedttea <twistedtteavr@gmail.com>
2023-12-08 17:30:40 -0600

    finished and reformatted

--- logic.py
+++ logic.py
@@ -1,117 +1,130 @@
 from gui import *
-from PyQt6.QtWidgets import *
+from PyQt6.QtWidgets import QMainWindow, QMessageBox, QApplication
 import os.path
 import csv
 
-class Logic(QMainWindow, Ui_MainWindow):
 
-    def __init__(self):
-        '''
-        Initializes
-        '''
+class Logic(QMainWindow, Ui_MainWindow):
+    def __init__(self) -> None:
+        """
+        Initializes the Logic class.
+        """
         super().__init__()
         self.setupUi(self)
         self.submitCsvButton.clicked.connect(self.getStudentScoresCSV)
         self.submitManualEntryButton.clicked.connect(self.getStudentScoresManual)
         self.manualEntryNextStudent.clicked.connect(self.inputStudents)
-        self.input_filename = ''
-        self.output_filename = ''
-        self.student_data = {}
+        self.input_filename: str = ""
+        self.output_filename: str = ""
+        self.student_data: dict = {}
 
     def getStudentScoresCSV(self) -> None:
+        """
+        Reads student scores from a CSV file, and writes results to the specified output file.
+        """
+
         try:
-            
             self.student_data = {}
-            self.input_filename=self.inputFileCheck()
-            self.output_filename=self.outputFileCheck()
-            with open(self.input_filename, 'r') as csvfile:
+            self.input_filename = self.inputFileCheck()
+            self.output_filename = self.outputFileCheck()
+            if self.output_filename == "donot":
+                raise IndexError
+            with open(self.input_filename, "r") as csvfile:
                 openedInputCSV = csv.DictReader(csvfile)
                 for row in openedInputCSV:
-                    self.student_data[(row['Name'])] = [row['Score']] 
+                    self.student_data[(row["Name"])] = [row["Score"]]
 
                 best = max(self.student_data.values())
-                
+
                 for row in self.student_data:
-                    self.student_data[row].append(self.grade_score(int(self.student_data[row][0] ),int(best[0])))
-            
-            with open(self.output_filename, 'w', newline='') as output_file:
+                    self.student_data[row].append(
+                        self.grade_score(int(self.student_data[row][0]), int(best[0]))
+                    )
+
+            with open(self.output_filename, "w", newline="") as output_file:
                 writer = csv.writer(output_file)
 
-                writer.writerow(['Name','Score','Grade'])
+                writer.writerow(["Name", "Score", "Grade"])
                 for name, data in self.student_data.items():
-                    writer.writerow([
-                    name,
-                    data[0],
-                    data[1]
-                    ])
-            self.label_3.setText(f"Results have been written \nto '{self.output_filename}'.")
+                    writer.writerow([name, data[0], data[1]])
+            self.label_3.setText(
+                f"Results have been written \nto '{self.output_filename}'."
+            )
 
         except FileNotFoundError:
-            if self.input_filename == '':
+            if self.input_filename == "":
                 self.label_3.setText("Please enter an \ninput filename.")
-            if self.input_filename == '' and self.output_filename == '':
+            if self.input_filename == "" and self.output_filename == "":
                 self.label_3.setText("Please enter an \noutput and input filename.")
-            elif self.output_filename == '':
+            elif self.output_filename == "":
                 self.label_3.setText("Please enter an \noutput filename.")
 
         except ValueError as e:
             self.label_3.setText(f"Error: {e}")
+        except IndexError:
+            pass
 
     def inputStudents(self) -> None:
-        '''
-        grabs and splits the entry line for student data, requires a space split
-        raises ValueError for both non alphanumeric inputs, and if the input has a non int for score
-        resets the error box for manual entry, and the manual entry box upon succsessfull addition of score
-        '''
+        """
+        Grabs and splits the entry line for student data, requires a space split
+        Raises ValueError for both non-alphanumeric inputs, and if the input has a non-integer for score
+        Resets the error box for manual entry, and the manual entry box upon succsessfull addition of score
+        """
         try:
             if not self.lineEdit_3.text().split()[0].isalnum():
                 raise ValueError
             if not self.lineEdit_3.text().split()[1].isnumeric():
                 raise ValueError
-            self.student_data[self.lineEdit_3.text().strip().split()[0]] = [self.lineEdit_3.text().strip().split()[1]]
-            self.manerror.setText('')
-            self.lineEdit_3.setText('')
+            self.student_data[self.lineEdit_3.text().strip().split()[0]] = [
+                self.lineEdit_3.text().strip().split()[1]
+            ]
+            self.manerror.setText("")
+            self.lineEdit_3.setText("")
+            self.lineEdit_3.setFocus()
 
         except IndexError:
-            self.manerror.setText('Please enter a name & score')
-        
+            self.manerror.setText("Please enter a name & score")
+
         except ValueError:
             if not self.lineEdit_3.text().split()[1].isnumeric():
-                self.manerror.setText('Score is not a number')
+                self.manerror.setText("Score is not a number")
             else:
-                self.manerror.setText('Please input only numbers & \nletters with a space delimiter')
-            
- 
+                self.manerror.setText(
+                    "Please input only numbers & \nletters with a space delimiter"
+                )
+
     def getStudentScoresManual(self) -> None:
+        """
+        Processes manually entered scores and writes results to specified output file.
+        """
         try:
             self.output_filename = self.outputFileCheckManual()
             best = max(self.student_data.values())
-            
+            if self.output_filename == "donot":
+                raise IndexError
             for row in self.student_data:
                 if len(self.student_data[row]) > 1:
                     raise IndexError
-                self.student_data[row].append(self.grade_score(int(self.student_data[row][0] ),int(best[0])))
-            print(self.student_data) 
-            
-            with open(self.output_filename, 'w', newline='') as output_file:
+                self.student_data[row].append(
+                    self.grade_score(int(self.student_data[row][0]), int(best[0]))
+                )
+
+            with open(self.output_filename, "w", newline="") as output_file:
                 writer = csv.writer(output_file)
 
-                writer.writerow(['Name','Score','Grade'])
+                writer.writerow(["Name", "Score", "Grade"])
                 for name, data in self.student_data.items():
-                    writer.writerow([
-                    name,
-                    data[0],
-                    data[1]
-                    ])
-            self.manerror.setText(f"Results have been written \nto '{self.output_filename}'.")
-            print(self.student_data)
-        
+                    writer.writerow([name, data[0], data[1]])
+            self.manerror.setText(
+                f"Results have been written \nto '{self.output_filename}'."
+            )
+
         except FileNotFoundError:
-            if self.input_filename == '':
+            if self.input_filename == "":
                 self.manerror.setText("Please enter an \ninput filename.")
-            if self.input_filename == '' and self.output_filename == '':
+            if self.input_filename == "" and self.output_filename == "":
                 self.manerror.setText("Please enter an \noutput and input filename.")
-            elif self.output_filename == '':
+            elif self.output_filename == "":
                 self.manerror.setText("Please enter an \noutput filename.")
 
         except ValueError as e:
@@ -120,76 +133,85 @@ class Logic(QMainWindow, Ui_MainWindow):
             pass
 
     def outputFileCheckManual(self) -> str:
-        fileName = self.lineEdit_4.text()
-        while os.path.isfile(fileName):
-            while True:
-                return fileName
-        return fileName
+        """
+        Checks for prexisting files, and asks the user if they want to overwritite the file.
+        Returns the outputs filename.
+        Only for manual entry method
+        """
+        self.output_filename = self.lineEdit_4.text()
+        if os.path.isfile(self.output_filename):
+            confirmation = QMessageBox.question(
+                self,
+                "File Overwrite Confirmation",
+                f"The file '{self.output_filename}' already exists. Do you want to overwrite it?",
+                QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No,
+                QMessageBox.StandardButton.No,
+            )
+            if confirmation == QMessageBox.StandardButton.No:
+                return "donot"  # Returns donot to stop overwrite
+
+        return self.output_filename
 
     def outputFileCheck(self) -> str:
-        fileName = self.lineEdit_2.text()
-        while os.path.isfile(fileName):
-            while True:
-                return fileName
-        return fileName
+        """
+        Checks for prexisting files, and asks the user if they want to overwritite the file.
+        Returns the outputs filename.
+        """
+        self.output_filename = self.lineEdit_2.text()
+        if os.path.isfile(self.output_filename):
+            confirmation = QMessageBox.question(
+                self,
+                "File Overwrite Confirmation",
+                f"The file '{self.output_filename}' already exists. Do you want to overwrite it?",
+                QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No,
+                QMessageBox.StandardButton.No,
+            )
+            if confirmation == QMessageBox.StandardButton.No:
+                return "donot"  # Returns donot to stop overwrite
 
+        return self.output_filename
 
     def inputFileCheck(self) -> str:
+        """
+        Prompts the user for an input filename until a valid one is used
+        Returns input filename
+        """
         while True:
             fileName = self.lineEdit.text()
             try:
                 with open(fileName) as inputFile:
                     break
             except:
-                self.label_3.setText('File does not exist!')
+                self.label_3.setText("File does not exist!")
                 break
-        print(fileName)
-        return fileName
-
-    def outputFileCheck(self) -> str:
-        fileName = self.lineEdit_2.text()
-        while os.path.isfile(fileName):
-            while True:
-                self.label_3.setText('File Overwritten')
-                return fileName
         return fileName
 
     def grade_score(self, score, best) -> str:
+        """
+        Determines the grade of student based on score and best score
+        Returns the grade
+        """
         if score >= best - 10:
-            return 'A'
+            return "A"
         elif score >= best - 20:
-            return 'B'
+            return "B"
         elif score >= best - 30:
-            return 'C'
+            return "C"
         elif score >= best - 40:
-            return 'D'
+            return "D"
         else:
-            return 'F'
- 
-    def searchScore(self,data_dict,name_to_search) -> str:
-        '''
+            return "F"
+
+    def searchScore(self, data_dict, name_to_search) -> str:
+        """
         uses hashtable lookup to search dictionary via key
         returns key upon succsess
         returns fail upon failure
-        '''
+
+        Currently unused
+        """
 
         if name_to_search in data_dict:
             return name_to_search
         else:
-            return 'fail'
-
-    # def extract_student_info(self,row) :
-    #     name_match = re.search(r'(?i)studentname', row)
-    #     score_match = re.search(r'(?i)score', row)
-    #
-    #     if name_match and score_match:
-    #         name_col = name_match.group()
-    #         score_col = score_match.group()
-    #
-    #         name = re.search(rf'(?i){name_col}\s*,\s*(?P<name>[\w\s]+)', row).group('name')
-    #         score = int(re.search(rf'(?i){score_col}\s*,\s*(?P<score>\d+)', row).group('score'))
-    #
-    #         return name, score
-    #     else:
-    #         raise ValueError("CSV file must contain 'studentname' and 'score' columns.")
-
+            return "fail"
